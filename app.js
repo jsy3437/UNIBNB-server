546241a4f19ccfe4f14f6c0835501a20762ee0a1
Uni <joseoyun12@gmail.com>
2023-03-09 04:35:05 +0900

    cors 에러를 해결하기 위한 코드 추가함

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ import express from 'express';
 const app = express();
 const port = 3308;
 
+const corsUrl = 'http://localhost:3000/';
+
 app.get('/', (req, res) => {
+	const allowOption = req.rawHeaders.findIndex((url) => url === corsUrl);
+
+	if (allowOption) {
+		res.setHeader('Access-Control-Allow-origin', '*');
+	}
+
 	res.send('hello!!');
 });
 
