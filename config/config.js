6fa6d83c154fe12d82f0f0332f9ad20a3603266f
Uni <joseoyun12@gmail.com>
2023-03-10 02:09:52 +0900

    개발모드 db config 모듈 생성함

---
+++ config/config.js
@@ -0,0 +1,15 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const config = {
+	development: {
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_DATABASE,
+		host: process.env.DB_HOST,
+		port: process.env.DB_PORT,
+		dialect: 'mariadb',
+	},
+};
+
+export default config;
