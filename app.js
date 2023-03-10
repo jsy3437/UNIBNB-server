279ba75fa7c20a59de25b112265a858dd6de6017
Uni <joseoyun12@gmail.com>
2023-03-11 00:33:09 +0900

    요청 body값을 읽기 위해 파싱하는 코드 추가함

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const port = 3308;
 const corsOptions = { origin: 'http://localhost:3000', credentials: true };
 
 app.use(cors(corsOptions));
+app.use(express.json());
 
 app.use('/auth', userRouter);
 
