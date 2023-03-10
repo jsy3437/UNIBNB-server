c4114dc3ffb1573c72c0e9b84cb80e628cb908ee
Uni <joseoyun12@gmail.com>
2023-03-11 00:46:23 +0900

    비밀번호를 암호화하고 db 데이터 추가코드 작성함

--- controller/user.js
+++ controller/user.js
@@ -1,13 +1,23 @@
+import bcrypt from 'bcrypt';
 import * as userRepo from '../data/user.js';
 
 export async function signup(req, res) {
 	const { email, firstname, lastname, birthday, password } = req.body;
 
+	const hashedPassword = bcrypt.hashSync(password, 8);
 	const overlapEmail = await userRepo.findByEmail(email);
 
 	if (overlapEmail) {
 		return res.status(202);
 	}
+
+	await userRepo.create({
+		email,
+		firstname,
+		lastname,
+		birthday,
+		password: hashedPassword,
+	});
 
 	return res.status(201);
 }
