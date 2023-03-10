912f0da2c7be6cda286dea1fa860ba514b91572a
Uni <joseoyun12@gmail.com>
2023-03-11 00:41:15 +0900

    회원가입을 위한 db 데이터 추가 함수 생성함

--- data/user.js
+++ data/user.js
@@ -1,5 +1,9 @@
 import db from '../models/index.js';
 
+export async function create(user) {
+	return db.User.create(user).then(console.log);
+}
+
 export async function findByEmail(email) {
 	return db.User.findOne({ where: { email: email } });
 }
